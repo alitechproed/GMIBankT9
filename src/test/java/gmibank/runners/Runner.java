8cad03d3e9418529815e19948102e7f551f4a023
zulas <zubeydeserap1221@gmail.com>
2020-11-30 21:33:24 +0100

    US_06_07 zubeyde

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -10,8 +10,8 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "gmibank/stepdefinitions",
-        tags = "@UserInfo",
-        dryRun = false
+        tags = "@US_030",
+        dryRun = true
 )
 public class Runner {
 
